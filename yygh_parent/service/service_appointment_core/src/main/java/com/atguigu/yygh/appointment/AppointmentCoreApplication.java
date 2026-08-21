de0cc5de8581b220cb3726db1ebbd14212c98b13
zht <3207858989@qq.com>
2026-08-21 10:41:47 +0800

    fix: 修复启动冲突

--- yygh_parent/service/service_appointment_core/src/main/java/com/atguigu/yygh/appointment/AppointmentCoreApplication.java
+++ yygh_parent/service/service_appointment_core/src/main/java/com/atguigu/yygh/appointment/AppointmentCoreApplication.java
@@ -1,14 +1,22 @@
 package com.atguigu.yygh.appointment;
 
+import com.atguigu.yygh.common.exception.GlobalExceptionHandler;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.atguigu"})
+@ComponentScan(
+        basePackages = {"com.atguigu"},
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = GlobalExceptionHandler.class
+        )
+)
 @EnableDiscoveryClient
 @EnableScheduling
 @MapperScan("com.atguigu.yygh.appointment.infrastructure.mapper")
